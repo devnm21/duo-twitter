9aebef9ecac5edbe22ec86872c1d539d88fe7ef5
Devakumar NM <pydev21@gmail.com>
2022-11-08 23:01:33 +0530

    feat: add auth utils for realm

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
-      <Banner />
     </ChakraProvider>
   );
 }
---
+++ utils/auth-utils.js
@@ -0,0 +1,17 @@
+import { Credentials } from 'realm-web';
+import realmApp from '../lib/realm';
+
+export const register = (email, password) => {
+    return realmApp.emailPasswordAuth.registerUser({ email, password });
+}
+
+export const verifyUser = (tokenId, token) => {
+    return realmApp.emailPasswordAuth.confirmUser({
+        token,
+        tokenId,
+    })
+}
+
+export const loginUser = (email, password) => {
+    return realmApp.logIn(Credentials.emailPassword(email, password));
+}
