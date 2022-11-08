29c174d1a6fcfe3518e13944e55fce1403a9baa5
Devakumar NM <pydev21@gmail.com>
2022-11-08 23:02:46 +0530

    feat: log in with realm

--- components/Auth/LoginModel.js
+++ components/Auth/LoginModel.js
@@ -4,15 +4,35 @@ import {
     ModalContent,
     ModalHeader,
     ModalBody,
-    ModalCloseButton, Flex, Container, Divider, Text, Input, Button,
+    ModalCloseButton, Flex, Container, Divider, Text, Input, Button, useBoolean, Alert, AlertTitle, AlertDescription,
 } from '@chakra-ui/react'
 import Link from "next/link";
+import {useState} from "react";
 
 // COMPONENTS
 import GoogleSignin from "./GoogleSignin";
 import AppleSignin from "./AppleSignin";
+import {loginUser} from "../../utils/auth-utils";
+import realmApp from "../../lib/realm";
 
 const LoginModel = ({isOpen, onClose, newRegisteredUser}) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useBoolean(false)
+    const login = async () => {
+        setLoading.toggle()
+        try {
+            await loginUser(email, password)
+            setLoading.toggle()
+            onClose()
+        } catch (e) {
+            setError(e.error)
+        }
+        setLoading.toggle()
+    }
 
     return <>
         <Modal isOpen={isOpen} onClose={onClose} size={'xl'}>
@@ -39,19 +59,32 @@ const LoginModel = ({isOpen, onClose, newRegisteredUser}) => {
 
                             <Flex flexDirection={'column'} gap={'1rem'}>
                                 <Input
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                     type={'text'}
                                     placeholder={'username or email or phone number'}
                                     size={'lg'}
                                     borderColor={'gray.700'}
                                 />
                                 <Input
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
                                     type={'password'}
                                     placeholder={'*********'}
                                     size={'lg'}
                                     borderColor={'gray.700'}
                                 />
-
+                                {error && <Alert status='error'>
+                                    <Flex direction={'column'}>
+                                        <AlertTitle w={'100%'} color={'red'} >Uh-Oh!</AlertTitle>
+                                        <AlertDescription color={'red'}>{error}</AlertDescription>
+                                    </Flex>
+                                </Alert>
+                                }
                                 <Button
+                                    onClick={login}
+                                    isLoading={loading}
+                                    isDisabled={loading}
                                     rounded={'full'}
                                     bg={'white'}
                                     color={'black'}
