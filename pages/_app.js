dee7a4315d77af43c980c1dadb84f7b441e1fb2f
Devakumar NM <pydev21@gmail.com>
2022-11-09 21:48:33 +0530

    feat: setup apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider } from "@apollo/client";
 
 // THEMES
 import { theme } from "../theme";
 
 // COMPONENTS
 import Banner from "../components/Auth/Banner";
+import client from "../lib/apollo-client";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+        <ApolloProvider client={client} >
+             <Component {...pageProps} />
+        </ApolloProvider>
     </ChakraProvider>
   );
 }
